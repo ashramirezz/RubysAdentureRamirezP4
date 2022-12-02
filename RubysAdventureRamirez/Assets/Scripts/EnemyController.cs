6bf21a037bba5c22dae683a8c0ce210a228991dc
ashramirezz <ashleymr146@gmail.com>
2022-12-02 11:46:03 -0800

    Projectiles
    Added the cog art and made it a prefab. I created a new script for projectiles and attacked it to the cog as well as edited the ruby script so she wouldn't collide with the cog

--- RubysAdventureRamirez/Assets/Scripts/EnemyController.cs
+++ RubysAdventureRamirez/Assets/Scripts/EnemyController.cs
@@ -11,6 +11,7 @@ public class EnemyController : MonoBehaviour
     Rigidbody2D rigidbody2D;
     float timer;
     int direction = 1;
+    bool broken = true;
 
     Animator animator;
     // Start is called before the first frame update
@@ -39,19 +40,26 @@ public class EnemyController : MonoBehaviour
         if (vertical)
         {
             position.y = position.y + Time.deltaTime * speed * direction; ;
-
             animator.SetFloat("Move X", 0);
             animator.SetFloat("Move Y", direction);
         }
         else
         {
             position.x = position.x + Time.deltaTime * speed * direction; ;
-
             animator.SetFloat("Move X", direction);
             animator.SetFloat("Move Y", 0);
         }
 
 
         rigidbody2D.MovePosition(position);
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        RubyController player = other.gameObject.GetComponent<RubyController>();
+
+        if (player != null)
+        {
+            player.ChangeHealth(-1);
+        }
     }
 }
